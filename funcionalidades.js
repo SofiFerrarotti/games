1368b44de739fabf42a3a86d10b78c5433e003c5
sofi <sofiferritta@gmail.com>
2024-06-09 20:02:54 -0300

    creo repo

---
+++ funcionalidades.js
@@ -0,0 +1,4 @@
+//Crear una funcion para que el usuario ingrese su nombre
+//Crear una funcion para que la pagina salude con su nombre
+// Mostrar imagenes de viejuegos en una lista y que el usuario pueda seleccionar uno o varios
+// Mostrar los juegos seleccionados en una lista (tipo carrito) de los juegos que seleccionó el usuario
